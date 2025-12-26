97cc650fb6aef1d392d30daf23228045683b1a86
Jatin Kaushik <Kaushikjatin532004@gmail.com>
2025-12-26 12:37:51 +0530

    everthing is done now

--- src/main/java/com/SmartContactManager/Smart/Contact/Manager/Final/Application.java
+++ src/main/java/com/SmartContactManager/Smart/Contact/Manager/Final/Application.java
@@ -1,13 +1,24 @@
 package com.SmartContactManager.Smart.Contact.Manager.Final;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application  {
 
+
+
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(e ->
+                System.setProperty(e.getKey(), e.getValue())
+        );
+
+        SpringApplication.run(Application.class, args);
+
+
 	}
 
 }
